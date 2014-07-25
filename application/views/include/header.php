314559b20c7979ea2ed6a9f737a09141a2d5445d
Daniel Hostetler <d.p.hostetler@gmail.com>
2014-07-26 02:48:02 +1200

    fixed typo in heading

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -7,7 +7,7 @@
    <meta name="keywords" content="">
    <meta name="author" content="">
 
-   <title>Inkmule | Customer T-shirts and Merchandise</title>
+   <title>Inkmule | Custom T-shirts and Merchandise</title>
 
    <link href="<?php echo base_url('assets/css/bootstrap.min.css') ?>" rel="stylesheet">
    <link href="<?php echo base_url('assets/css/font-awesome.css') ?>" rel="stylesheet">
