6bd2ac3b64d02ad153a23dc1699188338f3a6722
Daniel Hostetler <d.p.hostetler@gmail.com>
2014-07-26 02:29:02 +1200

    moved https forwarding back intohtaccess

--- application/controllers/pages.php
+++ application/controllers/pages.php
@@ -3,12 +3,6 @@ class Pages extends Main_Controller {
 
    public function index()
 	{
-	  //force https
-	  if (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] != "on") {
-        $url = "https://". $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
-	    redirect($url);
-	    exit;
-	  }
 	
       $this->load->view('include/header');
       $this->load->view('pages/frontpage');
@@ -18,12 +12,6 @@ class Pages extends Main_Controller {
  	public function view($page = 'home')
 	{
 	
-		//force https
-		  if (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] != "on") {
-	        $url = "https://". $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
-		    redirect($url);
-		    exit;
-		  }
 	
 	$this->load->view('include/header');	
 	
